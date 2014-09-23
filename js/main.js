df0dee69b4b74b97486b80e816a46026b8ab3932
Hektor Wallin <hektorw@gmail.com>
2014-09-23 09:23:43 +0200

    added vendor prefix fixes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,47 @@ require.config({
 require(['app'], function(App) {
   App.run();
 });
+
+
+
+
+// Prefixes
+(function() {
+  var w = window;
+  
+  // performance.now
+  var p = w.performance = w.performance || {};
+  p.now =
+    p.now ||
+    p.webkitNow ||
+    p.mozNow ||
+    p.msNow ||
+    function() {
+      return (new Date()).getTime();
+    };
+
+  // requestAnimationFrame
+  var rAF = 'equestAnimationFrame';
+  w['r'+rAF] =
+    w['r'+rAF] ||
+    w['webkitR'+rAF] ||
+    w['mozR'+rAF] ||
+    w['msR'+rAF] ||
+    function(cb) {
+      return setTimeout(function(){
+        cb(p.now());
+      }, 17);
+    };
+
+  // cancelAnimationFrame
+  var cAF = 'ancelAnimationFrame';
+  w['r'+cAF] =
+    w['r'+cAF] ||
+    w['webkitR'+cAF] ||
+    w['mozR'+cAF] ||
+    w['msR'+cAF] ||
+    function(id) {
+      return clearTimeout(id);
+    };
+
+}());
